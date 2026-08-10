76b1858b2b2ea584ac233dbb00992f46f5022347
Arthur97172 <arthur97172@outlook.com>
2026-08-10 22:04:17 +0800

    Update build25.sh

--- airoha/build25.sh
+++ airoha/build25.sh
@@ -27,11 +27,12 @@ PACKAGES=""
 
 # [核心系统]
 PACKAGES="$PACKAGES base-files uci ubus dropbear logd mtd bash htop curl wget ca-bundle ca-certificates"
-PACKAGES="$PACKAGES dnsmasq-full firewall4 nftables kmod-nft-offload"
+PACKAGES="$PACKAGES dnsmasq-full firewall4 nftables kmod-nft-offload fitblk"
 PACKAGES="$PACKAGES ip-full ipset iw ppp ppp-mod-pppoe -wpad-basic-mbedtls wpad-openssl libustream-openssl"
 
 # [无线驱动]
 PACKAGES="$PACKAGES kmod-cfg80211 kmod-mac80211 kmod-mt76-connac kmod-mt76-core kmod-hwmon-nct7802 rtl826x-firmware"
+PACKAGES="$PACKAGES airoha-en7581-mt7996-npu-firmware"
 PACKAGES="$PACKAGES kmod-mt7996-firmware kmod-mt7996-firmware-common kmod-mt7996e"
 
 #[其它驱动]
