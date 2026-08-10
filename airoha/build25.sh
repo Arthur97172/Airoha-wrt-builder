864ba73f2f1ed39b93a2853cc09b76a78131d90a
Arthur97172 <arthur97172@outlook.com>
2026-08-10 10:15:31 +0800

    Update build25.sh

--- airoha/build25.sh
+++ airoha/build25.sh
@@ -32,7 +32,10 @@ PACKAGES="$PACKAGES ip-full ipset iw ppp ppp-mod-pppoe -wpad-basic-mbedtls wpad-
 
 # [无线驱动]
 PACKAGES="$PACKAGES kmod-cfg80211 kmod-mac80211 kmod-mt76-connac kmod-mt76-core"
-PACKAGES="$PACKAGES kmod-mt7996-firmware kmod-mt7996-firmware-common kmod-mt7996e kmod-mtk-t7xx"
+PACKAGES="$PACKAGES kmod-mt7996-firmware kmod-mt7996-firmware-common kmod-mt7996e"
+
+#[以太网驱动]
+PACKAGES="$PACKAGES kmod-mtk-eth-soc kmod-mtk-ppe kmod-phy-aquantia kmod-phy-realtek kmod-phylink kmod-sfp ethtool iperf3 kmod-thermal lm-sensors"
 
 # [Web 界面]
 PACKAGES="$PACKAGES luci luci-base luci-i18n-base-zh-cn luci-mod-admin-full luci-i18n-firewall-zh-cn"
