e67f7b95516013fd27c7f9fe372325919ec18b18
Arthur97172 <arthur97172@outlook.com>
2026-08-11 08:42:03 +0800

    Update build25.sh

--- airoha/build25.sh
+++ airoha/build25.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-# Wrt 25.12.x Rockchip 构建脚本 (APK 格式)
+# Wrt 25.12.x Airoha 构建脚本 (APK 格式)
 # 在 imagebuilder 目录下运行
 
 # --- 接收外部参数 ---
-# 与 build24.sh 约定一致:$1=PROFILE, $2=ROOTFS_PARTSIZE
+# 与 build25.sh 约定一致:$1=PROFILE, $2=ROOTFS_PARTSIZE
 PROFILE=${1:-"gemtek_w1700k-ubi"}
+WRT_VERSION=${1:-"immortalwrt"}
 INCLUDE_DOCKER=${INCLUDE_DOCKER:-"no"}
 
 echo "Target Profile: $PROFILE"
 echo "Include Docker: $INCLUDE_DOCKER"
+echo "wrt version: $WRT_VERSION"
 
 # ============================================
 # 步骤1: 加载第三方插件配置
