8ff3c9d293267ef4785c0f31c097401a7dfc4620
Arthur97172 <arthur97172@outlook.com>
2026-09-07 16:20:10 +0800

    Update immortalwrt-custom-packages.sh

--- shell/immortalwrt-custom-packages.sh
+++ shell/immortalwrt-custom-packages.sh
@@ -82,7 +82,7 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-poweroffdevice luci-i18n-poweroffdevi
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-turboacc luci-i18n-turboacc-zh-cn"
 
 # easymesh无线组网(Easymesh)
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-eashmesh luci-i18n-easymesh-zh-cn"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-easymesh luci-i18n-easymesh-zh-cn"
 
 # taskplan定时任务 (taskplan)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-taskplan luci-i18n-taskplan-zh-cn"
