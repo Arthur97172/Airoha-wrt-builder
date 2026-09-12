894be163eeb3d473e8ceaa355f132503b135b22d
Arthur97172 <arthur97172@outlook.com>
2026-09-12 11:41:12 +0800

    Update package comments and enable some packages

--- shell/openwrt-custom-packages.sh
+++ shell/openwrt-custom-packages.sh
@@ -5,87 +5,90 @@
 #Others - DO NOT REMOVE
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-cpufreq luci-i18n-cpufreq-zh-cn"
 
-# cpu状态
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-cpu-status"
-
-#动态DNS
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES ddns-scripts-cloudflare luci-app-ddns luci-i18n-ddns-zh-cn"
-
-#samb文件共享
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-samba4 luci-i18n-samba4-zh-cn"
-
-#timecontrol上网时间控制
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-timecontrol luci-i18n-timecontrol-zh-cn"
-
-#watchdog看门狗
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES watchdog luci-app-watchdog luci-i18n-watchdog-zh-cn"
-
-#wireguard VPN控制面板
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-proto-wireguard qrencode"
-
-# adguardhome广告拦截 (adguardhome)
+# Adguardhome广告拦截 (adguardhome)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES adguardhome luci-app-adguardhome luci-i18n-adguardhome-zh-cn"
 
-#晶晨宝盒 (amlogic)
+# Amlogic晶晨宝盒 (amlogic)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-amlogic luci-i18n-amlogic-zh-cn"
 
-# argon主题 (argon)
+# Argon主题 (argon)
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-argon luci-app-argon-config luci-i18n-argon-config-zh-cn"
 
-# aurora主题 (aurora)
+# Aurora主题 (aurora)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-aurora-config luci-theme-aurora luci-i18n-aurora-config-zh-cn"
 
-# bandix-plus网络流量监控 (bandix-plus)
+# Bandix-plus网络流量监控 (bandix-plus)
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES bandix-plus luci-app-bandix-plus luci-i18n-bandix-plus-zh-cn"
 
-# bandix网络流量监控 (bandix)
+# Bandix网络流量监控 (bandix)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES bandix luci-app-bandix luci-i18n-bandix-zh-cn"
 
-# clashoo代理面板 (clashoo)
+# Clashoo代理面板 (clashoo)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES clashoo luci-app-clashoo luci-i18n-clashoo-zh-cn"
 
-# daede代理面板 (daede)
+# Cpu状态 (cpu-status)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-cpu-status"
+
+# Daede代理面板 (daede)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES dae daed luci-app-daede"
 
-# lucky内网穿透 (lucky)
+# Ddns 动态DNS (ddns)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES ddns-scripts-cloudflare luci-app-ddns luci-i18n-ddns-zh-cn"
+
+# Easymesh无线组网(easymesh)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-easymesh luci-i18n-easymesh-zh-cn"
+
+# Lucky内网穿透 (lucky)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES lucky luci-app-lucky luci-i18n-lucky-zh-cn"
 
-# MosDNS转发器 (MosDNS)
+# MosDNS转发器 (mosDNS)
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES mosdns luci-app-mosdns luci-i18n-mosdns-zh-cn"
 
-# netwizard网络设置向导 (netwizard)
+# Netspeedtest网速测试 (netspeedtest)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-netspeedtest luci-i18n-netspeedtest-zh-cn"
+
+# Netwizard网络设置向导 (netwizard)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-netwizard luci-i18n-netwizard-zh-cn"
 
-# nikki代理面板 (nikki)
+# Nikki代理面板 (nikki)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES nikki luci-app-nikki luci-i18n-nikki-zh-cn"
 
-# openclash代理面板 (openclash)
+# Openclash代理面板 (openclash)
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-openclash"
 
-# partexp分区扩容 (partexp)
+# Partexp分区扩容 (partexp)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-partexp luci-i18n-partexp-zh-cn"
 
-# passwall代理面板 (passwall)
+# Passwall代理面板 (passwall)
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-passwall luci-i18n-passwall-zh-cn"
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES sing-box chinadns-ng geoview shadowsocksr-libev-ssr-local shadowsocksr-libev-ssr-redir"
 
-# poweroffdevice关机 (poweroffdevice)
+# Poweroffdevice关机 (poweroffdevice)
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-poweroffdevice luci-i18n-poweroffdevice-zh-cn"
 
-# rtp2httpd流媒体转发 (rtp2httpd)
+# Rtp2httpd流媒体转发 (rtp2httpd)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES rtp2httpd luci-app-rtp2httpd luci-i18n-rtp2httpd-zh-cn"
 
-# run插件安装工具 (run)
+# Run插件安装工具 (run)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-run"
 
-# tailscaleVPN代理 (tailscale)
+# Samb文件共享 (samba4)
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-samba4 luci-i18n-samba4-zh-cn"
+
+# TailscaleVPN代理 (tailscale)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES tailscale luci-app-tailscale luci-i18n-tailscale-zh-cn"
 
-# turboacc网络加速(turboacc)
+# Turboacc网络加速(turboacc)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-turboacc luci-i18n-turboacc-zh-cn"
 
-# easymesh无线组网(Easymesh)
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-easymesh luci-i18n-easymesh-zh-cn"
-
-# taskplan定时任务 (taskplan)
+# Taskplan定时任务 (taskplan)
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-taskplan luci-i18n-taskplan-zh-cn"
+
+# Timecontrol上网时间控制 (timecontrol)
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-timecontrol luci-i18n-timecontrol-zh-cn"
+
+# Watchdog看门狗 (watchdog)
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES watchdog luci-app-watchdog luci-i18n-watchdog-zh-cn"
+
+# Wireguard VPN控制面板 (wireguard)
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-proto-wireguard qrencode"
